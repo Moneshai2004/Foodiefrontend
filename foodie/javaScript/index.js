823a84a8e47b304af30ffc259be7db495db20a2f
Moneshai2004 <140153126+Moneshai2004@users.noreply.github.com>
2023-10-09 18:22:38 +0530

    Add files via upload

---
+++ foodie/javaScript/index.js
@@ -0,0 +1,43 @@
+export function populateCards(mockData){
+    for(let i = 0;i <mockData.length;i++){
+        var cardContainer = document.createElement('div');
+        cardContainer.setAttribute('class','dish-container');
+        console.log(cardContainer);
+
+        var imgElement = document.createElement('img');
+        imgElement.setAttribute('class','dish-image');
+        imgElement.setAttribute('src',mockData[i].feature_image);
+        imgElement.setAttribute('alt','Restaurant Image');
+
+        var restaurantName = document.createElement('div')
+        restaurantName.setAttribute('class','dish-name');
+        restaurantName.appendChild(document.createTextNode(mockData[i].name))
+        console.log(restaurantName);
+
+        var restaurantDescription = document.createElement('div')
+        restaurantDescription.setAttribute('class','dish-description');
+        restaurantDescription.appendChild(document.createTextNode(mockData[i].cuisines));
+
+        var rating = document.createElement('div')
+        rating.setAttribute('class','rating');
+        rating.appendChild(document.createTextNode(mockData[i].rating));
+
+        var iElement = document.createElement('i');
+        iElement.setAttribute('class','fa fa-star');
+        iElement.appendChild(document.createTextNode(mockData[i].iElement));
+
+        var review = document.createElement('div')
+        review.setAttribute('class','reviews');
+        review.appendChild(document.createTextNode(mockData[i].reviews+" Reviews"));
+        
+        cardContainer.appendChild(imgElement);
+        cardContainer.appendChild(restaurantName);
+        cardContainer.appendChild(restaurantDescription);
+        cardContainer.appendChild(rating);
+        cardContainer.appendChild(review);
+        
+        console.log(cardContainer);
+
+        document.getElementById('menu-container').appendChild(cardContainer);
+    }
+}
